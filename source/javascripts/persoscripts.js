fb6e554453179e0b8e55370d6acaddf7da8d9365
serge cleoron <sc21417@gmail.com>
2021-09-10 20:49:30 +0200

    background images logo and others

--- source/javascripts/persoscripts.js
+++ source/javascripts/persoscripts.js
@@ -3,7 +3,7 @@
 $(window).scroll(function() {
   if ($(document).scrollTop() > 90) {
     $('.navbar-name').css("height", 60);
-    $('.navbar-name-brand img').css("height", 60)
+    $('.navbar-name-brand img').css("height", 54)
   } else {
     $('.navbar-name').css("height", 90);
     $('.navbar-name-brand img').css("height", 85)
